67142d3bd005824b5148860e897375d09079872b
Merwane98 <47528533+Merwane98@users.noreply.github.com>
2020-04-14 13:23:10 +0200

    Add files via upload

---
+++ ACHETEURaproposdenous.php
@@ -0,0 +1,330 @@
+<?php
+       session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+       <meta charset="utf-8">
+       <title>ECEBAY</title>
+       <style type="text/css">
+                            #menuderoulant, #menuderoulant ul{
+              padding:0px;
+              margin:0px;
+              list-style:none;
+              text-align:left;
+              }
+              #menuderoulant li{
+              display:inline-block;
+              position:relative;
+              border-radius:30px 30px 0 0;
+              }
+              #menuderoulant ul li{
+              display:inherit;
+              border-radius:0;
+              text-align:center;
+
+              }
+              
+              #menuderoulant ul{
+              position:absolute;
+              z-index: 1000;
+              max-height:0;
+              left: 0;
+              right: 0;
+              overflow:hidden;
+              -moz-transition: .8s all .3s;
+              -webkit-transition: .8s all .3s;
+              transition: .8s all .3s;
+              }
+              #menuderoulant li:hover ul{
+              max-height:15em;
+              }
+              /* background des liens menus */
+              #menuderoulant li:first-child{
+              background-color: red;
+              }
+              #menuderoulant li:nth-child(2){
+              background-color: blue;
+              }
+              /* background des liens sous menus */
+              #menuderoulant li:first-child li{
+              background: red;
+              }
+              #menuderoulant li:nth-child(2) li{
+              background: blue;
+              }
+              
+              /* background des liens menus et sous menus au survol */
+              #menuderoulant li:first-child:hover, #menuderoulant li:first-child li:hover{
+              background: red;
+              }
+              #menuderoulant li:nth-child(2):hover, #menuderoulant li:nth-child(2) li:hover{
+              background: blue;
+              }
+              
+              /* les a href */
+              #menuderoulant a{
+              text-decoration:none;
+              display:block;
+              padding:8px 32px;
+              color:#fff;
+              font-family:arial;
+              }
+              #menuderoulant ul a{
+              padding:8px 0;
+              }
+              #menuderoulant li:hover li a{
+              color:#fff;
+              text-transform:inherit;
+              }
+              #menuderoulant li:hover a, #menuderoulant li li:hover a{
+              color:#000;
+              }
+              #menuderoulantdroit, #menuderoulantdroit ul{
+              padding:0px;
+              margin:0px;
+              list-style:none;
+              text-align:right;
+              }
+              #menuderoulantdroit li{
+              display:inline-block;
+              position:relative;
+              border-radius:30px 30px 0 0;
+              }
+              #menuderoulantdroit ul li{
+              display:inherit;
+              border-radius:0;
+              text-align:center;
+
+              }
+              
+              #menuderoulantdroit ul{
+              position:absolute;
+              z-index: 1000;
+              max-height:0;
+              left: 0;
+              right: 0;
+              overflow:hidden;
+              -moz-transition: .8s all .3s;
+              -webkit-transition: .8s all .3s;
+              transition: .8s all .3s;
+              }
+              #menuderoulantdroit li:hover ul{
+              max-height:15em;
+              }
+              /* background des liens menus */
+              #menuderoulantdroit li:first-child{
+              background-color: #ffcc33;
+              }
+              #menuderoulantdroit li:nth-child(2){
+              background-color: green;
+              }
+              /* background des liens sous menus */
+              #menuderoulantdroit li:first-child li{
+              background: #ffcc33;
+              }
+              #menuderoulantdroit li:nth-child(2) li{
+              background:green;
+              }
+              
+              /* background des liens menus et sous menus au survol */
+              #menuderoulantdroit li:first-child:hover, #menuderoulantdroit li:first-child li:hover{
+              background:#ffcc33;
+              }
+              #menuderoulantdroit li:nth-child(2):hover, #menuderoulantdroit li:nth-child(2) li:hover{
+              background:green;
+              }
+              
+              /* les a href */
+              #menuderoulantdroit a{
+              text-decoration:none;
+              display:block;
+              padding:8px 32px;
+              color:#fff;
+              font-family:arial;
+              }
+              #menuderoulantdroit ul a{
+              padding:8px 0;
+              }
+              #menuderoulantdroit li:hover li a{
+              color:#fff;
+              text-transform:inherit;
+              }
+              #menuderoulantdroit li:hover a, #menuderoulantdroit li li:hover a{
+              color:#000;
+              }
+              #header{
+                     background-color: white;
+                     color: grey;
+                     text-align: center;
+                     padding: 5px;
+                     } 
+              #nav{
+                     line-height: 30px;
+                     background-color: #eeeeee;
+                     height: 100px;
+                     width: 1550px;
+                     float: left;
+                     padding: 5px;
+                     } 
+              #bande{
+                     line-height: 30px;
+                     background-color: black;
+                     height: 2px;
+                     width: auto;
+                     float: center;
+                     padding: 5px;
+                     } 
+              #section{
+                     background-color: #cccccc;
+                     width: auto;
+                     float: left;
+                     padding: 5px;
+                     height: 550px;
+                     } 
+              #bandeacceuil{
+                     background-color: white;
+                     color: grey;
+                     text-align: center;
+                     width: auto;
+                     float: center;
+                     padding: 10px;
+                     height: 300px;
+                     } 
+              #bandemeteo{
+                     background-color: white;
+                     color: grey;
+                     text-align: center;
+                     width: auto;
+                     float: right;
+                     padding: 10px;
+                     height: 300px;
+                     } 
+              #footer{
+                     background-color: black;
+                     color: white;
+                     clear: both;
+                     text-align: center;
+                     padding: 10px;
+                     }
+              #categoriegauche{
+                     background-color: white;
+                     color: grey;
+                     text-align: left;
+                     padding: 5px;
+                     } 
+              #categoriedroite{
+                     background-color: white;
+                     color: grey;
+                     text-align: right;
+                     padding: 5px;
+                     } 
+              .logogauche {
+                      float: left;
+                     padding-right: 30px;
+                     }      
+              .logodroite {
+                      float: right;
+                     padding-right: 5px;
+                     }      
+              .logocentre {
+                      float: center;
+                     padding-right: 5px;
+                     }      
+              div.float {
+                                    float: left;
+                                   }
+        </style>
+</head>
+<body>
+       <div id="header">
+               <img class="logocentre" src="titresite1.png" alt="titre" width="300" height="100">
+               <a href="PageAccueil.php"><img class="logodroite" src="logodeconnexion.png" alt="retour" width="50" height="50"></a>
+               <a href="ACHETEURpannier.php"><img class="logodroite" src="panier.png" alt="titre" width="50" height="50"></a>
+<div id="categoriegauche">    
+<?php
+       $mail=$_SESSION['a'];
+       $mysqli = new mysqli('localhost', 'root', '', 'eceamazon');
+       $mysqli->set_charset("utf8");
+       $requete = "SELECT * FROM Acheteur WHERE AdresseMail =  '$mail' ";
+       $resultat = $mysqli->query($requete);              
+       while ($ligne = $resultat->fetch_assoc()) 
+       { ?>
+               
+              <tr>
+                   <td><h2>Bonjour <?php echo $ligne['Civilite']?> <?php echo $ligne['Nom']?> <?php echo $ligne['Prenom']?>.</h2></td>                
+                                                                                 
+              </tr>
+       <?php
+       }
+              
+       $mysqli->close();
+       
+?>
+</div>
+       </div>
+       
+
+
+<div class="float">
+       <ul id="menuderoulant">
+       <li><a href="#">CATEGORIES</a>
+              <ul>
+                     <li><a href="ACHETEURPageLivre.php">Livres</a></li>
+                     <li><a href="ACHETEURpagemusique.php">Musique</a></li>
+                     <li><a href="ACHETEURpagevetement.php">Vêtements</a></li>
+                     <li><a href="ACHETEURpagesport.php">Sports et Loisir</a></li>
+              </ul>
+       </li>
+       <li><a href="#">PROMOTIONS</a>
+              <ul>
+                     <li><a href="ACHETEURpageventeflash.php">Ventes FLASH</a></li>
+                     <li><a href="ACHETEURpagesaintvalentin.php">Saint Valentin</a></li>
+                     <li><a href="ACHETEURpagenoel.php">Noël</a></li>
+                     <li><a href="ACHETEURpageperemere.php">Fête des pères/mères</a></li>
+              </ul>
+
+       </li>
+       
+</ul>
+</div>
+<ul id="menuderoulantdroit">
+       <li><a href="#">MON COMPTE</a>
+              <ul>
+                     <li><a href="pagecoordonnee.php">Mes Informations</a></li>
+              </ul>
+       </li>
+       <li><a href="#">CONTACT</a>
+              <ul>
+                     <li><a href="ACHETEURaproposdenous.php">A propos de nous</a></li>
+                     <li><a href="ACHETEURpagecontact.php">Nous contacter</a></li>
+                     <li><a href="ACHETEURFormulaire_Probleme.html">Signaler un problème</a></li>
+              </ul>
+
+       </li>
+</ul>
+<div id="bande">
+       </div>
+
+<center><h1>A propos de Nous</h1><br></center>
+      <h3> Le site EBAY ECE a été mis en place par 2 élèves de l'Ecole Centrale d'Électronique de Paris. Il s'agit d'un site d'E-commerce qui va révolutionner le monde du e-commerce. Avec son interface aérée, qui offre un aspect premium, les ingénieures ont fait tout pour que les utilisateurs se sentent le mieux possibles. Avec EBAY ECE, tout devient possible ! </h3>
+
+ <div id="bandeacceuil">
+       <h1>FONDATEUR D'EBAY ECE</h1>
+      <img class="logocentre" src="titresite1.jpg" alt="logosite" width="160" height="200">
+      <img class="logocentre" src="titresite1.jpg" alt="logosite" width="190" height="200">
+       <img class="logocentre" src="merwane.jpg" alt="logosite" width="150" height="200">
+       
+       </div>
+</p>
+
+
+
+</p>
+
+       </div>
+       <div id="footer">
+              Droit d'auteur | Copyright &copy; 2020, BR.Merwane & S.Louis-Henri
+       </div>
+</body>
+</html>
---
+++ ajouterpanier.php
@@ -0,0 +1,84 @@
+<?php
+session_start();
+?>
+<?php
+     $mail=$_SESSION['a'];
+     $mysqli = new mysqli('localhost', 'root', '', 'eceamazon');
+     $mysqli->set_charset("utf8");
+     //Tu recuperes l'id du contact
+     $id = $_GET["id"];
+     
+      
+
+     $requete = "SELECT * FROM Article WHERE ID=".$id;
+              $resultat = $mysqli->query($requete);              
+              while ($ligne = $resultat->fetch_assoc()) { 
+
+              	$titre = $ligne['Titre'];
+                $Mode1 = $ligne['Mode1'];
+                $Mode2 = $ligne['Mode2'];
+                $Mode3 = $ligne['Mode3'];
+     			$description = $ligne['Description'];
+     			$prix = $ligne['Prix'];
+                $image = $ligne['URLimage'];
+                $vendeur = $ligne['Vendeur'];
+                $quantite = $ligne['Quantité'];
+                $quantite2 = $ligne['Quantité'] - 1;
+                $quantite3 = $ligne['Quantité'] + 1;
+
+              	$sql2 = "SELECT * FROM panier";
+                    if ($id != "") {
+                        //on cherche le livre avec les paramètres titre et auteur
+                        $sql2 .= " WHERE ID='$id'";
+                      }
+                    $result = mysqli_query($mysqli, $sql2);
+                    if (mysqli_num_rows($result) == 0) {
+                        //le livre recherché n'existe pas
+                        echo "Erreur de connexion";
+                        $sql = "INSERT INTO panier VALUES ($id, '$titre','$Mode1','$Mode2','$Mode3', '$image', '$description', 1, $prix, '$vendeur', '$mail')";
+                        mysqli_query ($mysqli, $sql) or die ('Erreur SQL !'.$sql.'<br />'.mysqli_error($mysqli));
+                        
+
+                        $sql1 = "UPDATE Article SET Quantité = '$quantite2'  WHERE ID =".$id;
+                        mysqli_query ($mysqli, $sql1) or die ('Erreur SQL !'.$sql1.'<br />'.mysqli_error($mysqli));
+                        
+                      }
+
+                    else {
+                      //l'article est trouvé dans la base panier. On fait qté - 1 dans Article et qté + 1 dans panier
+                        $requete2 = "SELECT * FROM panier WHERE ID=".$id;
+                        $resultat2 = $mysqli->query($requete2);              
+                        while ($ligne2 = $resultat2->fetch_assoc()) {
+
+                            $qtepanier = $ligne2['Quantité'];
+                            $qtepanier2 = $ligne2['Quantité'] + 1;
+
+                            $sql3 = "UPDATE Article SET Quantité = '$quantite2'  WHERE ID =".$id;
+                            mysqli_query ($mysqli, $sql3) or die ('Erreur SQL !'.$sql3.'<br />'.mysqli_error($mysqli));
+                      
+
+                            $sql4 = "UPDATE panier SET Quantité = '$qtepanier2'  WHERE ID =".$id;
+                            mysqli_query ($mysqli, $sql4) or die ('Erreur SQL !'.$sql4.'<br />'.mysqli_error($mysqli));
+                        }
+
+                    }
+                    
+              
+            }
+
+             
+            header('Location: ACHETEURPageLivre.php');
+
+                     
+
+   
+       $mysqli->close();    
+     
+     //Requete SQL pour supprimer le contact dans la base
+     //mysqli_query($mysqli, "INSERT INTO panier () VALUES (".$id.")");
+     //Et la tu rediriges vers ta page contacts.php pour rafraichir la liste
+     //echo 'Article ajouté au panier. Veuillez cliquez <a href="ACHETEURPageAccueil.php">ici</a> pour retourner à la page des articles';
+
+     ?>
+
+     
