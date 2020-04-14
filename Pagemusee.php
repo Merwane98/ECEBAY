9677620da5982593e92d1106f3795e42c358e52d
Merwane98 <47528533+Merwane98@users.noreply.github.com>
2020-04-14 14:35:56 +0200

    Update Pagemusee.php

--- Pagemusee.php
+++ Pagemusee.php
@@ -302,7 +302,7 @@
 <?php
               $mysqli = new mysqli('localhost', 'root', '', 'eceamazon');
               $mysqli->set_charset("utf8");
-              $requete = 'SELECT * FROM Article WHERE Type LIKE "musique%" ';
+              $requete = 'SELECT * FROM Article WHERE Type LIKE "Bon%" ';
               $resultat = $mysqli->query($requete);
               while ($ligne = $resultat->fetch_assoc()) {
 
