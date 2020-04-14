2b2fdc84fda37055ff8bd3f41dc99f298549b1a5
LHS10 <63642318+LHS10@users.noreply.github.com>
2020-04-14 13:11:39 +0200

    Update aproposdenous_vendeur.php

--- aproposdenous_vendeur.php
+++ aproposdenous_vendeur.php
@@ -245,7 +245,7 @@ session_start();
 <body>
        <div id="header">
               <a href="PageAccueil.php"><img class="logogauche" src="logodeconnexion.png" alt="logosite" width="50" height="50"></a>
-               <img class="logocentre" src="titresite.png" alt="titre" width="400" height="70">
+               <img class="logocentre" src="titresite1.png" alt="titre" width="400" height="100">
                <?php
        $mail=$_SESSION['a'];
        $mysqli = new mysqli('localhost', 'root', '', 'eceamazon');
