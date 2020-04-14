4b181e13013408949181ff8b35f56f7d7fbd62e6
LHS10 <63642318+LHS10@users.noreply.github.com>
2020-04-14 14:23:12 +0200

    Update ACHETEURaproposdenous.php

--- ACHETEURaproposdenous.php
+++ ACHETEURaproposdenous.php
@@ -5,7 +5,7 @@
 <html>
 <head>
        <meta charset="utf-8">
-       <title>ECEBAY</title>
+       <title>ECE EBAY</title>
        <style type="text/css">
                             #menuderoulant, #menuderoulant ul{
               padding:0px;
@@ -307,10 +307,10 @@
        </div>
 
 <center><h1>A propos de Nous</h1><br></center>
-      <h3> Le site EBAY ECE a été mis en place par 2 élèves de l'Ecole Centrale d'Électronique de Paris. Il s'agit d'un site d'E-commerce qui va révolutionner le monde du e-commerce. Avec son interface aérée, qui offre un aspect premium, les ingénieures ont fait tout pour que les utilisateurs se sentent le mieux possibles. Avec EBAY ECE, tout devient possible ! </h3>
+      <h3> Le site ECE EBAY a été mis en place par 2 élèves de l'Ecole Centrale d'Électronique de Paris. Il s'agit d'un site d'E-commerce qui va révolutionner le monde du e-commerce. Avec son interface aérée, qui offre un aspect premium, les ingénieures ont fait tout pour que les utilisateurs se sentent le mieux possibles. Avec ECE EBAY, tout devient possible ! </h3>
 
  <div id="bandeacceuil">
-       <h1>FONDATEUR D'EBAY ECE</h1>
+       <h1>FONDATEUR D'ECE EBAY</h1>
       <img class="logocentre" src="titresite1.jpg" alt="logosite" width="160" height="200">
       <img class="logocentre" src="titresite1.jpg" alt="logosite" width="190" height="200">
        <img class="logocentre" src="merwane.jpg" alt="logosite" width="150" height="200">
