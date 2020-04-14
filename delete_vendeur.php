a8c5ab8a4249933bfb13b651000d90bad9a853a9
LHS10 <63642318+LHS10@users.noreply.github.com>
2020-04-14 12:23:47 +0200

    Add files via upload

---
+++ delete_vendeur.php
@@ -0,0 +1,14 @@
+<?php
+   
+     $mysqli = new mysqli('localhost', 'root', '', 'eceamazon');
+     $mysqli->set_charset("utf8");
+     //Tu recuperes l'id du contact
+     $mail = $_GET["id"];
+
+     //Requete SQL pour supprimer le contact dans la base
+     $sql = "DELETE FROM Vendeur WHERE AdresseMail = '$mail'";
+     mysqli_query($mysqli, $sql);
+     //Et la tu rediriges vers ta page contacts.php pour rafraichir la liste
+     header('Location: listetousvendeur.php');
+     //echo 'Vendeur supprimé. Cliquez <a href="listetousvendeur.php">ici</a> pour retourner à la liste des vendeurs';
+?>
