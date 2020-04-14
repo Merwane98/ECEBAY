b7fd0fabb77b64e2ab6718e84ff2913f4834dc92
Merwane98 <47528533+Merwane98@users.noreply.github.com>
2020-04-14 14:18:47 +0200

    Update delete_annonce_panier.php

--- delete_annonce_panier.php
+++ delete_annonce_panier.php
@@ -1,11 +1,28 @@
 <?php
-   
-     $mysqli = new mysqli('localhost', 'root', '', 'eceamazon');
-     $mysqli->set_charset("utf8");
+
+$mysqli = new mysqli('localhost', 'root', '', 'eceamazon');
+$mysqli->set_charset("utf8");
      //Tu recuperes l'id du contact
-     $id = $_GET["id"];
-     //Requete SQL pour supprimer le contact dans la base
-     mysqli_query($mysqli, "DELETE FROM Panier WHERE ID = ".$id);
+$id = $_GET["id"];
+     //Réajout bdd article quantité
+$requete = "SELECT * FROM panier WHERE ID=".$id;
+$resultat = $mysqli->query($requete);              
+while ($ligne = $resultat->fetch_assoc()) { 
+
+	$qtepanier = $ligne['Quantité'];
+
+	$requete2 = "SELECT * FROM Article WHERE ID=".$id;
+	$resultat2 = $mysqli->query($requete2);              
+	while ($ligne2 = $resultat2->fetch_assoc()) {
+
+		$quantité = $ligne2['Quantité'] + $ligne['Quantité'];
+		$sql0 = "UPDATE Article SET Quantité = '$quantité'  WHERE ID =".$id;
+		mysqli_query ($mysqli, $sql0) or die ('Erreur SQL !'.$sql0.'<br />'.mysqli_error($mysqli));}}
+
+		//Requete SQL pour supprimer le contact dans la base
+		mysqli_query($mysqli, "DELETE FROM Panier WHERE ID = ".$id);
+		$mysqli->close(); 
+
      //Et la tu rediriges vers ta page contacts.php pour rafraichir la liste
-     header('Location: ACHETEURpannier.php');
-?>
+		header('Location: ACHETEURpannier.php');
+		?>
