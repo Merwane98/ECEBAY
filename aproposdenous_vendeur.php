a1bac89582dd7bc460e33f3bc7c9c2af55e152d0
LHS10 <63642318+LHS10@users.noreply.github.com>
2020-04-14 13:17:38 +0200

    Update aproposdenous_vendeur.php

--- aproposdenous_vendeur.php
+++ aproposdenous_vendeur.php
@@ -302,10 +302,10 @@ session_start();
        </div>
 
 <center><h1>A propos de Nous</h1><br></center>
-      <h3> Le site ECE AMAZON a été mis en place par 3 élève de l'Ecole Centrale d'Électronique de Paris. Il s'agit d'un site d'E-commerce qui va révolutionner le monde du e-commerce. Avec son interface aérée, qui offre un aspect premium, les ingénieures ont fait tout pour que les utilisateurs se sentent le mieux possibles. Avec ECE AMAZON, tout devient possible ! </h3>
+      <h3> Le site ECE EBAY a été mis en place par 3 élève de l'Ecole Centrale d'Électronique de Paris. Il s'agit d'un site d'E-commerce qui va révolutionner le monde du e-commerce. Avec son interface aérée, qui offre un aspect premium, les ingénieures ont fait tout pour que les utilisateurs se sentent le mieux possibles. Avec ECE EBAY, tout devient possible ! </h3>
 
  <div id="bandeacceuil">
-       <h1>FONDATEUR D' ECE AMAZON</h1>
+       <h1>FONDATEUR D' ECE EBAY</h1>
       <img class="logocentre" src="vishnu.jpg" alt="logosite" width="160" height="200">
       <img class="logocentre" src="kishor.jpg" alt="logosite" width="190" height="200">
        <img class="logocentre" src="merwane.jpg" alt="logosite" width="150" height="200">
