63b963927fd92116b7af4e1eeb061a13cd4a1f45
LHS10 <63642318+LHS10@users.noreply.github.com>
2020-04-16 17:58:41 +0200

    Update pageventeflash.php

--- pageventeflash.php
+++ pageventeflash.php
@@ -230,7 +230,7 @@
 </head>
 <body>
        <div id="header">
-              <a href="formulaireadmin.php"><img class="logogauche" src="logoadmin.png" alt="logosite" width="50" height="50"></a>
+              <a href="formulaireadmin.php"><img class="logogauche" src="logoadmin1.png" alt="logosite" width="50" height="50"></a>
                <img class="logocentre" src="logopromotion.png" alt="titre" width="500" height="100">
                <a href="PageAccueil.php"><img class="logodroite" src="retour.png" alt="retour" width="50" height="50"></a>
        </div>
