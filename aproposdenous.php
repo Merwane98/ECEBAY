1f87174866c9f435f3916fca5353392a8fb34d55
LHS10 <63642318+LHS10@users.noreply.github.com>
2020-04-16 18:11:03 +0200

    Update aproposdenous.php

--- aproposdenous.php
+++ aproposdenous.php
@@ -227,7 +227,7 @@
 </head>
 <body>
        <div id="header">
-              <a href="formulaireadmin.php"><img class="logogauche" src="logoadmin.png" alt="logosite" width="50" height="50">
+              <a href="formulaireadmin.php"><img class="logogauche" src="logoadmin1.png" alt="logosite" width="50" height="50">
                <img class="logocentre" src="titresite1.png" alt="titre" width="500" height="100">
                <a href="PageAccueil.php"><img class="logodroite" src="retour.png" alt="retour" width="50" height="50"></a>
        </div>
@@ -278,8 +278,7 @@
 
  <div id="bandeacceuil">
        <h1>FONDATEUR D' ECE AMAZON</h1>
-      <img class="logocentre" src="vishnu.jpg" alt="logosite" width="160" height="200">
-      <img class="logocentre" src="kishor.jpg" alt="logosite" width="190" height="200">
+      <img class="logocentre" src="lh.jpg" alt="logosite" width="160" height="200">
        <img class="logocentre" src="merwane.jpg" alt="logosite" width="150" height="200">
        
        </div>
