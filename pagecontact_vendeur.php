efa4ef940a72d05db701eb999b6e9e6b8e290cba
LHS10 <63642318+LHS10@users.noreply.github.com>
2020-04-19 18:18:21 +0200

    Update pagecontact_vendeur.php

--- pagecontact_vendeur.php
+++ pagecontact_vendeur.php
@@ -41,25 +41,25 @@ session_start();
               }
               /* background des liens menus */
               #menuderoulant li:first-child{
-              background-color: #F9980C;
+              background-color: red;
               }
               #menuderoulant li:nth-child(2){
-              background-color: #F9980C;
+              background-color: blue;
               }
               /* background des liens sous menus */
               #menuderoulant li:first-child li{
-              background:#F9980C;
+              background:red;
               }
               #menuderoulant li:nth-child(2) li{
-              background:#F9980C;
+              background:blue;
               }
               
               /* background des liens menus et sous menus au survol */
               #menuderoulant li:first-child:hover, #menuderoulant li:first-child li:hover{
-              background:#F9980C;
+              background:red;
               }
               #menuderoulant li:nth-child(2):hover, #menuderoulant li:nth-child(2) li:hover{
-              background:#F9980C;
+              background:blue;
               }
               
               /* les a href */
@@ -114,25 +114,25 @@ session_start();
               }
               /* background des liens menus */
               #menuderoulantdroit li:first-child{
-              background-color: #F9980C;
+              background-color: #ffcc33;
               }
               #menuderoulantdroit li:nth-child(2){
-              background-color: #F9980C;
+              background-color: green;
               }
               /* background des liens sous menus */
               #menuderoulantdroit li:first-child li{
-              background:#F9980C;
+              background:#ffcc33;
               }
               #menuderoulantdroit li:nth-child(2) li{
-              background:#F9980C;
+              background:green;
               }
               
               /* background des liens menus et sous menus au survol */
               #menuderoulantdroit li:first-child:hover, #menuderoulantdroit li:first-child li:hover{
-              background:#F9980C;
+              background:#ffcc33;
               }
               #menuderoulantdroit li:nth-child(2):hover, #menuderoulantdroit li:nth-child(2) li:hover{
-              background:#F9980C;
+              background:green;
               }
               
               /* les a href */
@@ -192,7 +192,7 @@ session_start();
                      height: 300px;
                      } 
               #footer{
-                     background-color: #F9980C;
+                     background-color: black;
                      color: white;
                      clear: both;
                      text-align: center;
